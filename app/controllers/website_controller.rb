3c7c981b7205703d08c6a4a681e9f5568920aec9
Tayyab064 <muhammad.tayyabmukhtar@yahoo.com>
2021-02-12 19:40:07 +0500

    2nd commit

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -1,10 +1,22 @@
 class WebsiteController < ApplicationController
 
+	def signin
+
+	end
+
 	def index
 
 	end
 
 	def course
+
+	end
+
+	def teacher
+
+	end
+
+	def specific_course
 
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'website#index'
   get 'course' => 'website#course' , as: "course"
+  get 'signin' => 'website#signin' , as: 'signin'
+  get 'instructor_:id' => 'website#teacher' , as: 'teacher_profile'
+  get 'course_:id' => 'website#specific_course' , as: 'specific_course'
 end
